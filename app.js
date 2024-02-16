4875023a21081f49e06b57daaeb2764a28c226f3
Izzah_Salam <izzah.bscssef20@iba-suk.edu.pk>
2024-02-16 22:41:11 +0500

    practicing event target

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const about = document.querySelector(".about")
 const articles = document.querySelectorAll(".content");
 
 about.addEventListener("click",(event)=>{
-    console.log(event.target);
+   const id= event.target.dataset.id;
+   if(id){
+    // remove active class from other btns
+
+    btns.forEach((btn)=>{
+        btn.classList.remove("active")
+        event.target.classList.add("active")
+
+    })
+    // hide other article except the current
+    articles.forEach((article)=>{
+        article.classList.remove("active")
+
+    })
+
+    const element = document.getElementById(id)
+    element.classList.add("active")
+
+
+   }
 
 })
